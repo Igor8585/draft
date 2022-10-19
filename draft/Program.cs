ae9535c6556f440bdf17d8a0891a5bc9d670f14b
Igor <irushoff@gmail.com>
2022-10-19 20:02:33 +0300

    Добавьте файлы проекта.

---
+++ draft/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace draft
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte age = 37;
+
+            ChangeAge(age);
+
+            Console.WriteLine(age);
+        }
+
+        static void ChangeAge(byte age)
+        {
+            age = (byte)Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
