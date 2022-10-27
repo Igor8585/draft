cde1528d91249ef9b8c27272f24b1590333010cc
Igor <irushoff@gmail.com>
2022-10-27 20:28:57 +0300

    Задание 7.1.10

--- draft/Program.cs
+++ draft/Program.cs
@@ -1,19 +1,36 @@
 ﻿using System;
 
 
-//Реализуйте конструктор, заполняющий поля для следующего класса:
-class Obj
-{
-	private string name;
-	private string owner;
-	private int length;
-	private int count;
 
-	public Obj(string name, string ownerName, int objLength, int count)
-	{
-		this.name = name;
-		owner = ownerName;
-		length = objLength;
-		this.count = count;
-	}
+//Для класса DerivedClass создайте 2 конструктора: один, принимающий 2 параметра — name и description, второй — принимающий 3 параметра name, description и counter.
+//Старайтесь написать оптимальный вариант с учетом полученных знаний.
+
+class BaseClass
+{
+    protected string Name;
+
+    public BaseClass(string name)
+    {
+        Name = name;
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    //private static string name;
+
+    public string Description;
+
+    public int Counter;
+
+    public DerivedClass(string name, string description) : base(name)
+    {
+        Description = description;
+    }
+
+    public DerivedClass(string name, string description, int counter) : this(name, description)
+    {
+        //Description = description;
+        Counter = counter;
+    }
 }
