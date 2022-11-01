3899d4399e2d83cc0fad1408d9beeffb8d7aed92
Igor <irushoff@gmail.com>
2022-11-01 10:42:05 +0300

    Задание 7.6.12

--- draft/Program.cs
+++ draft/Program.cs
@@ -1,25 +1,38 @@
 ﻿using System;
 
-//Установите ограничения на универсальные типы в классе Car. Такие, чтобы поле Engine могло принимать тип ElectricEngine и GasEngine,
-//а параметр newPart метода ChangePart мог бы принимать только типы частей машины (Battery, Differential, Wheel).
+//С учётом полученных знаний по наследованию обобщений, дополните схему классов автомобиля, добавив классы для электромобиля и бензинового — ElectricCar и GasCar.
 //
-//Для этого вам может понадобиться использовать один из ранее изученных принципов ООП.
+//Подумайте, какой класс или классы можно сделать абстрактными. Сделайте абстрактными их и их члены (по возможности).
 
 class ElectricEngine : BaseEngine { }
 class GasEngine : BaseEngine { }
 class Battery : BaseChangePart { }
 class Differential : BaseChangePart { }
 class Wheel : BaseChangePart { }
-class BaseEngine { }
-class BaseChangePart { }
-class Car<TEngine> where TEngine : BaseEngine
+abstract class BaseEngine { }
+abstract class BaseChangePart { }
+abstract class Car<TEngine> where TEngine : BaseEngine
 {
     public TEngine Engine;
-    public virtual void ChangePart<TPart>(TPart newPart) where TPart : BaseChangePart
-    {
-    }
+    public abstract void ChangePart<TPart>(TPart newPart) where TPart : BaseChangePart;
 
     public static void Main()
     {
+    }
+}
+
+class ElectricCar : Car<ElectricEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
+    }
+}
+
+class GasCar : Car<GasEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
     }
 }
