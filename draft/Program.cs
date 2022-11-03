f92869a29871af031ef2cfcf6348796f96ceafab
Igor <irushoff@gmail.com>
2022-11-03 20:04:01 +0300

    Итоговое задание

--- draft/Program.cs
+++ draft/Program.cs
@@ -1,38 +1,158 @@
 ﻿using System;
 
-//С учётом полученных знаний по наследованию обобщений, дополните схему классов автомобиля, добавив классы для электромобиля и бензинового — ElectricCar и GasCar.
-//
-//Подумайте, какой класс или классы можно сделать абстрактными. Сделайте абстрактными их и их члены (по возможности).
-
-class ElectricEngine : BaseEngine { }
-class GasEngine : BaseEngine { }
-class Battery : BaseChangePart { }
-class Differential : BaseChangePart { }
-class Wheel : BaseChangePart { }
-abstract class BaseEngine { }
-abstract class BaseChangePart { }
-abstract class Car<TEngine> where TEngine : BaseEngine
+abstract class Delivery
 {
-    public TEngine Engine;
-    public abstract void ChangePart<TPart>(TPart newPart) where TPart : BaseChangePart;
+    public string Address;
+}
 
+class HomeDelivery : Delivery
+{
+    /* ... */
+}
+
+class PickPointDelivery : Delivery
+{
+    /* ... */
+}
+
+class ShopDelivery : Delivery
+{
+    /* ... */
+}
+
+abstract class Product
+{
+    public string Id;
+    public string Name;
+    public string Description;
+
+    public Product(string id)
+    {
+        Id = id;
+    }
+}
+
+class Appliances : Product
+{
+    public string newId;
+    public int Power;
+    public Appliances(string id) : base(id)
+    {
+        newId = id;
+    }
+}
+
+class Electronics : Product
+{
+    public string newId;
+    private decimal diagonal;
+    public decimal Diagonal {
+        get
+        {
+            if(diagonal < 32 || diagonal > 75)
+                {
+                    return 0;
+                }
+            else { return diagonal; }
+        }
+
+        set 
+        {
+            if(diagonal < 32 || diagonal > 75)
+            {
+                Console.WriteLine("Введите диагональ в пределах от 32 до 75");
+            }
+            else { diagonal = value; }
+        }
+      }
+
+    public Electronics(string id) : base(id)
+    {
+        newId = id;
+    }
+}
+
+class HouseholdTool : Product
+{
+    public string newId;
+    public char EnergySavingClass;
+    public HouseholdTool(string id) : base(id)
+    {
+        newId = id;
+    }
+}
+
+//abstract class Basket<TProduct> where TProduct : Product
+class Basket<TProduct> where TProduct : Product
+{
+    private TProduct[] basket;
+    
+    public Basket(TProduct[] basket)
+    {
+        this.basket = basket;
+    }
+
+    public TProduct this[int index]
+    {
+        get {
+            if (index > 0 && index < basket.Length)
+            {
+                return basket[index];
+            }
+            else { return null; }
+        } 
+        private set
+        {
+            if (index > 0 && index < basket.Length)
+            {
+                basket[index] = value;
+            }
+        }
+    }
+}
+
+class Order<TDelivery> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    public Order()
+    {
+
+    }
+
+    // ... Другие поля
+}
+
+class Program
+{
     public static void Main()
     {
-    }
-}
+        var product = new Electronics[]
+        {
+            new Electronics("1")
+            {
+                Description = "TV",
+                Diagonal = 55,
+                Name = "Горизонт"
+            }
+        };
 
-class ElectricCar : Car<ElectricEngine>
-{
-    public override void ChangePart<TPart>(TPart newPart)
-    {
+        Order<HomeDelivery> hd = new Order<HomeDelivery>();
 
-    }
-}
+        Basket<Electronics> userBasket = new Basket<Electronics>(product);
 
-class GasCar : Car<GasEngine>
-{
-    public override void ChangePart<TPart>(TPart newPart)
-    {
+        Electronics electr = userBasket[0];
+
+        Console.WriteLine(electr);
 
     }
 }
