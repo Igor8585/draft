0745154e4641b7dec29111d76a178286f031c1b5
Igor <irushoff@gmail.com>
2022-11-08 21:46:45 +0300

    Задание 8.1.4

--- draft/Program.cs
+++ draft/Program.cs
@@ -1,158 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-abstract class Delivery
+class InfoHdd
 {
-    public string Address;
-}
+    public string NameDisk { get; }
+    public decimal DiskSpace { get; }
+    public decimal Total { get; }
 
-class HomeDelivery : Delivery
-{
-    /* ... */
-}
-
-class PickPointDelivery : Delivery
-{
-    /* ... */
-}
-
-class ShopDelivery : Delivery
-{
-    /* ... */
-}
-
-abstract class Product
-{
-    public string Id;
-    public string Name;
-    public string Description;
-
-    public Product(string id)
+    public InfoHdd(string namedisk, decimal space, decimal total)
     {
-        Id = id;
+        NameDisk = namedisk;
+        DiskSpace = space;
+        Total = total;
     }
-}
-
-class Appliances : Product
-{
-    public string newId;
-    public int Power;
-    public Appliances(string id) : base(id)
-    {
-        newId = id;
-    }
-}
-
-class Electronics : Product
-{
-    public string newId;
-    private decimal diagonal;
-    public decimal Diagonal {
-        get
-        {
-            if(diagonal < 32 || diagonal > 75)
-                {
-                    return 0;
-                }
-            else { return diagonal; }
-        }
-
-        set 
-        {
-            if(diagonal < 32 || diagonal > 75)
-            {
-                Console.WriteLine("Введите диагональ в пределах от 32 до 75");
-            }
-            else { diagonal = value; }
-        }
-      }
-
-    public Electronics(string id) : base(id)
-    {
-        newId = id;
-    }
-}
-
-class HouseholdTool : Product
-{
-    public string newId;
-    public char EnergySavingClass;
-    public HouseholdTool(string id) : base(id)
-    {
-        newId = id;
-    }
-}
-
-//abstract class Basket<TProduct> where TProduct : Product
-class Basket<TProduct> where TProduct : Product
-{
-    private TProduct[] basket;
     
-    public Basket(TProduct[] basket)
-    {
-        this.basket = basket;
-    }
-
-    public TProduct this[int index]
-    {
-        get {
-            if (index > 0 && index < basket.Length)
-            {
-                return basket[index];
-            }
-            else { return null; }
-        } 
-        private set
-        {
-            if (index > 0 && index < basket.Length)
-            {
-                basket[index] = value;
-            }
-        }
-    }
 }
 
-class Order<TDelivery> where TDelivery : Delivery
+public class Folder
 {
-    public TDelivery Delivery;
-
-    public int Number;
-
-    public string Description;
-
-    public void DisplayAddress()
-    {
-        Console.WriteLine(Delivery.Address);
-    }
-
-    public Order()
-    {
-
-    }
-
-    // ... Другие поля
+    public List<string> Files { get; set; } = new List<string>();
 }
 
 class Program
 {
     public static void Main()
     {
-        var product = new Electronics[]
-        {
-            new Electronics("1")
-            {
-                Description = "TV",
-                Diagonal = 55,
-                Name = "Горизонт"
-            }
-        };
 
-        Order<HomeDelivery> hd = new Order<HomeDelivery>();
+    }
 
-        Basket<Electronics> userBasket = new Basket<Electronics>(product);
+    Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
 
-        Electronics electr = userBasket[0];
-
-        Console.WriteLine(electr);
-
+    public void CreateFolder(string name)
+    {
+        Folders.Add(name, new Folder());
     }
 }
