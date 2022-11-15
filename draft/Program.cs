d975b025f71fd70f44833cbe243596f23578d5fc
Igor <irushoff@gmail.com>
2022-11-15 21:20:03 +0300

    Задание 9.1.4

--- draft/Program.cs
+++ draft/Program.cs
@@ -2,14 +2,18 @@
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-//Создайте класс исключения Exception и добавьте в свойство Data дату создания исключения.
+//Создайте класс исключения Exception и переопределите его свойство Message, а также свойство HelpLink, добавив в него ссылку на внешний ресурс.
 
 class Draft
 {
     static void Main()
     {
-        Exception exception = new Exception();
+        Exception exception = new Exception("Собственное исключение");
 
         exception.Data.Add("Дата создания исключения : ", DateTime.Now);
+
+        exception.HelpLink = "www.google.ru";
+
+        //exception.Message.("");
     }
 }
